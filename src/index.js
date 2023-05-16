0258ebd2997650dbfb6f26bf5b73209f121841ca
LeandroYara <l.yara@uniandes.edu.co>
2023-05-16 09:26:47 -0500

    Push final

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {IntlProvider} from 'react-intl';
+import localeEsMessages from "./locales/es";
+import localeEnMessages from "./locales/en";
 
-import JobsList from "./components/jobslist";
+import JobsList from "./components/jobsList";
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+let localeMessage;
+let lang = navigator.language || navigator.userLanguage;
+if (lang === "en")
+{
+    localeMessage = localeEnMessages;
+}
+else if (lang === "es")
+{
+    localeMessage = localeEsMessages;
+}
+
+ReactDOM.render(
+        <IntlProvider locale={lang} messages= {localeMessage}>
+                <JobsList/>
+        </IntlProvider>, document.getElementById("root")
+);
